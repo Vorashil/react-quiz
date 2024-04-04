74797e44bbf78af4811f7e2e2542380c82c6a00a
Vorashil Farzaliyev <vofarzaliyev@gmail.com>
2024-04-04 18:03:59 +0100

    added all tests for react router but they are failing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import Greeting from "./Greeting";
+import {Route, Routes, BrowserRouter, Link} from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import MyPage from "./pages/MyPage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
-    return (
-        <>
+
+    const NavBarItem = ({name, href}: { name: string, href: string }) => {
+        return (
+            <li className={"p-2 mx-2 border-2 border-blue-400"}>
+                <Link to={href}>
+                    {name}
+                </Link>
+            </li>
+        )
+    }
+
+    const NavBar = ({children}: { children: React.ReactNode }) => {
+        return (
             <div>
-                <ul>
-                    <li>Home Page</li>
-                    <li>My Page</li>
-                    <li>Login</li>
+                <ul className={"flex flex-row"}>
+                    {children}
                 </ul>
             </div>
+        )
+
+    }
+
+    return (
+        <>
+            <BrowserRouter>
+                <NavBar>
+                    <NavBarItem name={"Home"} href={"/"}/>
+                    <NavBarItem name={"My Page"} href={"/mypage"}/>
+                    <NavBarItem name={"Log in"} href={"/login"}/>
+                </NavBar>
+
+                <Routes>
+                    <Route path={"/"} element={<HomePage/>}/>
+                    <Route path={"/mypage"} element={<MyPage/>}/>
+                    <Route path={"/login"} element={<LoginPage/>}/>
+                </Routes>
+            </BrowserRouter>
+
             <Greeting name={"oyren.dev"}/>
         </>
     );
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,19 +1,39 @@
 import {render, screen, within} from "@testing-library/react";
 import App from "../App";
+import userEvent from "@testing-library/user-event";
+describe('App component', () => {
+    test('Navigation bar is rendered', () => {
 
-test('Navigation bar is rendered', () => {
+        // GIVEN
+        render(<App/>)
 
-    render(<App/>)
+        // WHEN
 
-    // WHEN
+        // THEN
 
-    // THEN
+        const homeLink = screen.getByText('home', {exact: false});
+        const myPageLink = screen.getByText('My Page');
+        const loginLink = screen.getByText('Login');
 
-    const homeLink = screen.getByText('home' , {exact: false});
-    const myPageLink = screen.getByText('My Page');
-    const loginLink = screen.getByText('Login');
+        expect(homeLink).toBeInTheDocument();
+        expect(myPageLink).toBeInTheDocument();
+        expect(loginLink).toBeInTheDocument();
+    })
 
-    expect(homeLink).toBeInTheDocument();
-    expect(myPageLink).toBeInTheDocument();
-    expect(loginLink).toBeInTheDocument();
+    test('Navigation to the my page', () => {
+
+        // GIVEN
+        render(<App/>)
+
+        // WHEN
+        const myPageNavLink = screen.getByText('My page', {exact: false});
+        userEvent.click(myPageNavLink);
+
+        // THEN
+        expect(screen.getByText('Welcome to My Page')).toBeInTheDocument();
+    })
+
+    test('Navigation to the login page', () => {})
+
+
 })
